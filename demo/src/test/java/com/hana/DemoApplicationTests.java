15bc51b61adea6707e991bc08f6f875597f19556
JeongHeum Byun <quswjdgma83@gmail.com>
2024-03-25 10:09:36 +0900

    스프링 초기세팅

---
+++ demo/src/test/java/com/hana/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.hana;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class DemoApplicationTests {
+    @Test
+    void contextLoads() {
+        log.info("Log Test");
+    }
+}햣 
